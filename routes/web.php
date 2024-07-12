673f20febb43859d035356acf6d577dbe53899ed
Cleverson Sucalit <cleversonsucalit@gmail.com>
2024-07-12 09:54:10 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebPostController;
-
+// test
 Auth::routes();
 
 Route::get('/', [WebPostController::class, 'index'])->name('home');
